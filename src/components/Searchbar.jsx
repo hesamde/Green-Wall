a4b880e1a85785b1c9256acdc4c5f24373118003
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-09-14 13:24:14 +0200

    redesign pages

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,36 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import "./Searchbar.css"
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import "./Searchbar.css";
 
-const Searchbar = ({setAllProducts}) => {
-    const [name, setName] = useState('');
+const Searchbar = ({ setAllProducts }) => {
+  const [name, setName] = useState("");
+  const { isLoggedIn } = useContext(AuthContext);
 
-    useEffect(()=> {
-        console.log("useEffect");
-        axios.get(`${process.env.REACT_APP_API_URL}/product/name/${name}`)
+  useEffect(() => {
+    console.log("useEffect");
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/product/name/${name}`)
 
-        .then((response) => {
-            console.log(response.data);
-            setAllProducts(response.data)
-        })
-        .catch((error) => console.error(error));
-    },[name])
+      .then((response) => {
+        console.log(response.data);
+        setAllProducts(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, [name]);
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        setName(e.target.value);
-    };
+  const handleChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
 
-    return (
-        <div className='input-container'>
-            <input
-                className='search-b'
-                type="search"
-                value={name}
-                placeholder="Search..."
-                onChange={handleChange}/>
-        </div>
-    );
-}
+  return (
+    <div className="input-container">
+      <input
+        className="search-input"
+        type="search"
+        value={name}
+        placeholder="Search..."
+        onChange={handleChange}
+      />
+      {isLoggedIn && (
+        <Link to="/createproduct" className="cp-button">
+          Create Product
+        </Link>
+      )}
+    </div>
+  );
+};
 
 export default Searchbar;
